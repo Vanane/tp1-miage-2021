428e16c3c0aa663390c890a23f969b1713a927fb
tp <tp@tp>
2021-03-27 18:00:37 +0100

    Ajout Test 24h mais import manquant

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,28 @@
 package com.acme.todolist;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.acme.todolist.domain.TodoItem;
 
 @SpringBootTest
 class TodolistApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testGetTodoList()
+	{
+		TodoItem ti = new TodoItem("12345", Instant.now().plus(1, ChronoUnit.DAYS), "Test RG1 date < 24h ");
+		
+		assertTrue(ti.finalContent().startsWith(TodoItem.LATE));
 	}
 
 }
